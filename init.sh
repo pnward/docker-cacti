e3167be68a748a3a879d542bcd971bf85014f2b8
Christoph Dwertmann <cdwertmann@gmail.com>
2016-10-05 17:12:05 +1100

    Revert "add timezone support"
    This reverts commit 2931af70e19673d96a5f016853cea6ac81331e7d.

--- init.sh
+++ init.sh
@@ -3,5 +3,4 @@ sed -i "s/\$database_hostname = \"localhost\";/\$database_hostname = \"mysql\";/
 sed -i "s/\$database_default = \"cacti\";/\$database_default = \"$MYSQL_DATABASE\";/g" /var/www/localhost/htdocs/cacti/include/config.php
 sed -i "s/\$database_username = \"cactiuser\";/\$database_username = \"$MYSQL_USER\";/g" /var/www/localhost/htdocs/cacti/include/config.php
 sed -i "s/\$database_password = \"cactiuser\";/\$database_password = \"$MYSQL_PASSWORD\";/g" /var/www/localhost/htdocs/cacti/include/config.php
-sed -i "s/date.timezone = UTC/date.timezone = $TIMEZONE/g" /etc/php5/php.ini
 exec supervisord --nodaemon --configuration /etc/supervisord.conf
